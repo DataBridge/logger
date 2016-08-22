d4647beee70991fe2c060d52c8b15b4386be93a7
monsieurwave <tensu.wave@gmail.com>
2016-08-21 23:11:49 -0400

    Fixed style-errors

--- src/server.js
+++ src/server.js
@@ -1,95 +1,94 @@
 /**
- * @author Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
- * @flow
- */
+* @author Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
+* @flow
+*/
 
- import Chance from 'chance';
- import Koa from 'koa';
- import now from 'performance-now';
- import { merge } from 'lodash';
- import parse from 'co-body';
+import Chance from 'chance';
+import Koa from 'koa';
+import now from 'performance-now';
+import { merge } from 'lodash';
+import parse from 'co-body';
 
- import FileBackend from './backends/file';
- import Logger from './Logger';
+import FileBackend from './backends/file';
+import Logger from './Logger';
 
- const app = new Koa();
- const randomGenerator = new Chance();
- const backend = new FileBackend('./all.log');
- const logger = new Logger('databridge-logger', backend, 'main-logging-server');
+const app = new Koa();
+const randomGenerator = new Chance();
+const backend = new FileBackend('./all.log');
+const logger = new Logger('databridge-logger', backend, 'main-logging-server');
 
- logger.log('logger-prepare');
+logger.log('logger-prepare');
 
-//Build ctx.log method 
+// Build ctx.log method
 app.use(async (ctx, next) => {
-	const requestId = randomGenerator.guid();
+  const requestId = randomGenerator.guid();
   ctx.log = (eventId, details, level) => { // eslint-disable-line no-param-reassign
-  	const allDetails = merge(details, {
-  		requestId,
-  	});
-  	logger.log(eventId, allDetails, level);
+    const allDetails = merge(details, {
+      requestId,
+    });
+    logger.log(eventId, allDetails, level);
   };
 
-//Log the new request
-ctx.log('new-request', {
-	ip: ctx.request.ip,
-	length: ctx.request.length
-});
-await next();
-});
-
-// Filter out non-post requests 
-app.use(async (ctx, next) => {
-	if (ctx.request.method === 'POST') {
-		await next();
-	} else {
-		ctx.log('non-post-request', {
-			method: ctx.request.method,
-			url: ctx.request.url
-		}, 'warning');
-	} 
+  // Log the new request
+  ctx.log('new-request', {
+    ip: ctx.request.ip,
+    length: ctx.request.length
+  });
+  await next();
 });
 
-//Mesure request duration 
+// Filter out non-post requests
 app.use(async (ctx, next) => {
-	const start = now();
-	await next();
-	const end = now();
-	ctx.log('end-request', {
-		requestDuration: end - start
-	});
+  if (ctx.request.method === 'POST') {
+    await next();
+  } else {
+    ctx.log('non-post-request', {
+      method: ctx.request.method,
+      url: ctx.request.url
+    }, 'warning');
+  }
 });
 
+// Mesure request duration
 app.use(async (ctx, next) => {
-	const startParsing = now();
-	ctx.body = await parse.json(ctx.request, { limit: '10kb' });
-	const endParsing = now();
-	ctx.log('body-parsed', {
-		parsingDuration: endParsing - startParsing,
-	});
-	await next();
+  const start = now();
+  await next();
+  const end = now();
+  ctx.log('end-request', {
+    requestDuration: end - start
+  });
 });
 
 app.use(async (ctx, next) => {
-	const startStoring = now();
+  const startParsing = now();
+  ctx.body = await parse.json(ctx.request, { limit: '10kb' });
+  const endParsing = now();
+  ctx.log('body-parsed', {
+    parsingDuration: endParsing - startParsing,
+  });
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  const startStoring = now();
   // TODO sanitize/verify log entries
   const messages = ctx.body;
   await backend.store(messages);
   const endStoring = now();
   ctx.log('logs-stored', {
-  	storingDuration: endStoring - startStoring
+    storingDuration: endStoring - startStoring
   });
   await next();
 });
 
 app.use(async ({ response }, next) => {
-	response.status = 200;
-	response.body = '';
-	await next();
+  response.status = 200;
+  response.body = '';
+  await next();
 });
 
 const port = process.env.NODE_PORT || 3000;
 logger.log('logger-start-listen', {
-	port
+  port
 });
 app.listen(port);
-
